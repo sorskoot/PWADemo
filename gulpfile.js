b58405f3fd3c28f265f2bca99319d27686ec661a
sorskoot <mental_c@hotmail.com>
2016-11-29 09:16:10 +0100

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+﻿var zip = require("gulp-zip");
+var gulp = require('gulp');
+
+gulp.task("deploy", function () {
+  return gulp.src(["**"])
+    .pipe(zip("deploy.zip"))
+    .pipe(gulp.dest("./"));
+});
+
+const fs = require('fs');
+
+gulp.task('compile', function () {
+  //gulp.src(['**/*.ts', '!**/node_modules/**'])
+  //  .pipe(typescript())
+  //  .pipe(gulp.dest('./'))
+});
